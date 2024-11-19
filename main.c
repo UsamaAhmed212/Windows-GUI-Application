329546689dbc3f2a94b01bd56969e9bf1fe2ed2b
SpikE <usamaahmed212aaa@gmail.com>
2024-11-19 21:52:21 +0600

    Create a font that supports Bengali characters |& and &| Draw Text (Bangla characters) in the window using Unicode

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <windows.h>
 #include "resource.h" // Include your resource header
+#include <stdio.h>
 
 // Function prototype for window procedure
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -102,6 +103,86 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
             DeleteObject(hPen5px);
             DeleteObject(hPen2px);
 
+            // Define starting position for text
+            int startX = 150;
+            int startY = 120;
+            int lineHeight = 30; // Space between lines
+
+            // Set text background color to #EBAB00
+            SetBkColor(hdc, RGB(235, 171, 0));  // #EBAB00
+            SetBkMode(hdc, OPAQUE);             // Ensure background is painted
+
+            // Draw each spike on a new line
+            TextOut(hdc, startX, startY, "spike a", 7);
+            TextOut(hdc, startX, startY + lineHeight, "spike b", 7);
+            TextOut(hdc, startX, startY + (lineHeight * 2), "spike c", 7);
+            TextOut(hdc, startX, startY + (lineHeight * 3), "spike d", 7);
+
+
+            // Set text background color to #EBAB00
+            SetBkColor(hdc, RGB(235, 171, 0));  // #EBAB00
+            SetBkMode(hdc, OPAQUE);             // Ensure background is painted
+
+            // RECT rect = {15, 15, 200, 100};
+            // DrawText(hdc, "Hello, World!", -1, &rect, DT_WORDBREAK | DT_VCENTER | DT_SINGLELINE);
+            
+
+            // Create a font that supports Bangla
+            HFONT hFont = CreateFontW(
+                30,                     // Height of font
+                0,                      // Width of font
+                0,                      // Angle of escapement
+                0,                      // Orientation angle
+                FW_NORMAL,              // Font weight
+                FALSE,                  // Italic
+                FALSE,                  // Underline
+                FALSE,                  // Strikeout
+                DEFAULT_CHARSET,        // Character set identifier
+                OUT_OUTLINE_PRECIS,     // Output precision
+                CLIP_DEFAULT_PRECIS,    // Clipping precision
+                CLEARTYPE_QUALITY,      // Output quality
+                DEFAULT_PITCH,          // Pitch and family
+                L"Nirmala UI"           // Font name (Nirmala UI supports Bangla)
+            );
+
+            // Select the font into the DC
+            HFONT hOldFont = (HFONT)SelectObject(hdc, hFont);
+
+            // Set up the rectangle for drawing
+            RECT rect = {15, 15, 200, 100};
+
+            // Draw Text in the window using Unicode
+            const wchar_t *text = L"ক";  // Bangla character "ক"
+
+            // Draw the character with background color #EBAB00
+            SetBkColor(hdc, RGB(235, 171, 0));  // Set background color
+            SetBkMode(hdc, OPAQUE);             // Enable opaque background
+            DrawTextW(hdc, text, -1, &rect, DT_WORDBREAK | DT_VCENTER | DT_SINGLELINE);
+
+            // Calculate width of the text for offset
+            SIZE textSize;
+            GetTextExtentPoint32W(hdc, text, wcslen(text), &textSize);
+            rect.left += textSize.cx;  // Move position after drawing
+
+            SetBkMode(hdc, TRANSPARENT);  // Enable transparent background
+
+            // Draw a space
+            text = L" "; // Set text to a space
+            GetTextExtentPoint32W(hdc, text, wcslen(text), &textSize);
+            rect.left += textSize.cx;  // Move position after space
+
+            // Draw the space
+            DrawTextW(hdc, text, -1, &rect, DT_WORDBREAK | DT_VCENTER | DT_SINGLELINE);
+
+            // Now draw the text "kh"
+            text = L"kh";  // Set text to "kh"
+            SetBkMode(hdc, TRANSPARENT);  // Enable transparent background
+            DrawTextW(hdc, text, -1, &rect, DT_WORDBREAK | DT_VCENTER | DT_SINGLELINE);
+
+            // Reset background mode if needed for other drawing operations
+            SetBkMode(hdc, TRANSPARENT);
+
+            
             EndPaint(hwnd, &ps);
             return 0;
         }
